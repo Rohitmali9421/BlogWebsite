70e8d70d42c0b5b3c30a61a873bc8d5d2cde4c1b
Rohit Mali <rohitsmali9421@gmail.com>
2024-06-05 22:43:39 +0530

    created signup component

--- src/Components/index.js
+++ src/Components/index.js
@@ -2,9 +2,13 @@ import Header from "./Header/Header"
 import Footer from "./Footer/Footer"
 import Container from "./container/Container"
 import LogoutBtn from "./Header/LogoutBtn"
+import Button from "./Button"
+import Input from "postcss/lib/input"
 export {
     Header,
     Footer,
     Container,
-    LogoutBtn
+    LogoutBtn,
+    Button,
+    Input
 }
